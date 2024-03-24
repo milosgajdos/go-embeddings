d67a5cffc28d86039908029975633120ec1dcf07
Milos Gajdos <milosthegajdos@gmail.com>
2024-03-24 16:07:23 +0000

    update: add tests for decoding base64 encoded emeddings
    Signed-off-by: Milos Gajdos <milosthegajdos@gmail.com>

--- embeddings_test.go
+++ embeddings_test.go
@@ -1,6 +1,9 @@
 package embeddings
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestToFloat32(t *testing.T) {
 	e := Embedding{
@@ -18,5 +21,49 @@ func TestToFloat32(t *testing.T) {
 		if exp[i] != f {
 			t.Fatalf("expected %v, got %v", exp, got)
 		}
+	}
+}
+
+func TestBase64Decode(t *testing.T) {
+	t.Parallel()
+	testCases := []struct {
+		name    string
+		given   string
+		exp     []float64
+		wantErr bool
+	}{
+		{
+			name:    "valid",
+			given:   "H4XrUbgeCUAAAAAAAABFwESLbOf7QI9A",
+			exp:     []float64{3.14, -42.0, 1000.123},
+			wantErr: false,
+		},
+		{
+			name:    "invalid",
+			given:   "garbage",
+			wantErr: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			embBase64 := Base64(tc.given)
+			got, err := embBase64.Decode()
+			if err != nil {
+				if !tc.wantErr {
+					t.Fatalf("unexpected error: %v", err)
+				}
+				return
+			}
+			// no error, but we expect one
+			if tc.wantErr {
+				t.Fatal("expected error")
+			}
+			if !reflect.DeepEqual(got, tc.exp) {
+				t.Fatalf("expected: %v, got: %v", tc.exp, got)
+			}
+		})
 	}
 }
